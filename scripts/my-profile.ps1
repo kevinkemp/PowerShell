9c73ef9fbcb8c76ca26403a90b376be0e5832644
Kevin Kemp <kmkemp11@gmail.com>
2014-07-17 11:28:06 -0700

    moved away from using a separate folder for github

--- scripts/my-profile.ps1
+++ scripts/my-profile.ps1
@@ -2,12 +2,11 @@ $env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"
 . (Resolve-Path ~/Documents/WindowsPowershell/ssh-agent-utils.ps1)
 
 # Load posh-git example profile
-. 'C:\dev\GitHub\posh-git\profile.example.ps1'
+. 'C:\dev\posh-git\profile.example.ps1'
 
 $global:GitPromptSettings.EnableFileStatus = $false #speed up posh-git
 
 New-PSDrive su filesystem 'C:\dev\serviceu'
-New-PSDrive gh filesystem 'C:\dev\GitHub'
 New-PSDrive mod fileSystem ~/Documents\WindowsPowerShell/Modules
 
 # Add ability to remember last directory to cd
